4853a3eb8113b18028538b4b0d45afcc74c554e8
Willie Tong <tongsinyin@gmail.com>
2017-01-19 16:53:08 +0800

    responses received

--- app/js/components/Global/Main.jsx
+++ app/js/components/Global/Main.jsx
@@ -1,6 +1,45 @@
 import React, { Component } from 'react';
+import {Apis} from 'graphenejs-ws';
 
 export default class Main extends Component {
+  constructor(prop) {
+    super();
+    this.state = {
+      assets: []
+    }
+  }
+
+  componentWillMount() {
+    this._loadAssets("A", 100);
+  }
+
+  _loadAssets(start, count) {
+    Apis.instance("wss://bitshares.openledger.info/ws", true).init_promise.then((res) => {
+      console.log("Connected to: ", res[0]);
+      Apis.instance().db_api().exec("set_subscribe_callback", [this.updateListener, true]);
+      Apis.instance().db_api().exec("list_assets", [start, count])
+        .then(assets => {
+          assets.forEach(asset => {
+            console.log(asset)
+          })
+        })
+    });
+  }
+
+  updateListener(object) {
+    console.log("set_subscribe_callback:\n", object);
+  }
+
+/*
+Apis.instance().db_api().exec("list_assets", [start, count])
+  .then(assets => {
+    assets.forEach(asset => {
+      console.log(asset)
+    })
+  })
+*/
+
+
   render() {
     return (
       <div className='Main'>
